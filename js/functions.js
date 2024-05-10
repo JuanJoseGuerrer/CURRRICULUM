cc4595ab2c5509d4c53bba6f30bb905b4beb2987
juanjoseguerrer <jjguerreroa@ufpso.edu.co>
2024-05-10 15:05:36 -0500

    cargando

--- js/functions.js
+++ js/functions.js
@@ -33,15 +33,7 @@ console.log("mi edad es: " + edad);
 // === estrictamente comparado (valor y el tipo de dato)
 // >, >=, <, <=, <>
 
-function load_page(){
-    Swal.fire({
-        imageUrl: "https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg",
-        //imageHeight: 1500,
-        imageAlt: "A tall image",
-        showConfirmButton: false,
-        timer: 2000
-      });
-}
+
 
 function send_form(){
     
@@ -75,5 +67,128 @@ function send_form(){
         document.getElementById("print").innerText = "Su nombre es " + name + " " + last_name;
     }
 
+
+}
+
+
+ //arrays
+ var array_semana = ["Lunes", "martes", "miercoles", "jueves", "viernes"];
+ var array_gral = [1,2,2.5, "ABC","SOY WILDER"];
+ var array_num = [1,2,3,4,5,6,7,8,9,];
+
+ var array_multi = [
+     {name:"Wilder", age:32, color:"verde"},
+     {name:"anyi", age:33, color:"rosa"},
+     {name:"celeste" , age:3, color:"rosa"},
+     {name:"antonella", age:2, color:"blanco"}
+ ];
+
+ var json_ejm ={
+     name: "wilder",
+     lastname: "duarte",
+     phone: 3158667751,
+     email:"waduarten@ufpso.edu.co"
+ }
+ 
+ console.log(array_semana);
+ console.log(array_gral);
+ console.log(array_num);
+ console.log(array_multi);
+ console.log(json_ejm);
+
+ function action(){
+     //alert(array_num);
+     swal.fire("" + array_num);
+     console.log(array_num.lengt);
+
+     //var result = 0;
+     //for(let i = 0; i<array_num.length; i++){
+     //   result = result + array_num[i];
+     //}
+
+
+     //con for
+     var total = 0;
+     for(var j = 0; j < array_multi.length; j++){
+        console.log(total = (array_multi[j].age) + total);
+        
+     }
+     alert(total);
+
+
+     //con while
+     var total_dos = 0;
+     var w = 0;
+     while(w<array_multi.length){
+        total_dos = array_multi[w].age + total_dos;
+        w++;
+     }
+
+     alert(total_dos);
+
+     document.getElementById("result").innerHTML = "<strong> El resultado es: " + result + "</strong>";
+     document.getElementById("result").style.color = "green";
+
+ }
+
+function load_page(){
+    Swal.fire({
+        imageUrl: "https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg",
+        //imageHeight: 1500,
+        imageAlt: "A tall image",
+      showConfirmButton: false,
+      timer: 2000
+      });
+
+        document.getElementById("text").innerText = "el array a calcular es" + array_num;
+        document.getElementById("impresion").value = array_num;
+        
+}
+
+//var array_numerico = []
+
+function eliminar(){
+    let array_delete = array_num.pop();
+    console.log(array_delete);
+    console.log(array_num);
+    document.getElementById("impresion").value = array_num;
+
+}
+
+function agregar(){
+    let num = document.getElementById("num").value
+    let array_add = array_num.push(num);
+    console.log(array_add);
+    document.getElementById("impresion").value = array_num;
+    console.log(array_num)
+}
+
+function reves(){
+    document.getElementById("impresion").value = array_num.reverse();
+}
+
+function limpiar(){
+    document.getElementById("num").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("last_name").value = "";
+    document.getElementById("result").value = "";
+}
+
+function primNum(){
+    let array_primNum = array_num.push(array_num[0]);
+    console.log(array_primNum);
+    console.log(array_num);
+    document.getElementById("impresion").value = array_num;
+    
+    
+}
+
+function ultNum(){
+    let valor = array_num.reverse()
+    let array_ultNum = array_num.push(array_num[0]);
+    console.log(array_ultNum);
+    console.log(array_num);
+    document.getElementById("impresion").value = array_num;
+    
     
 }
